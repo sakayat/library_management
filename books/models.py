4ab949da0a9121b509408a63d910b7446e589fd0
sakayat <sakhyatkabir1@gmail.com>
2024-06-16 08:22:57 +0600

    added books, category, reviews model added

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from category.models import CategoryModel
+
+
+# Create your models here.
+class BooksModel(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    borrowing_price = models.DecimalField(decimal_places=2, max_digits=12)
+    image = models.ImageField(upload_to="uploads/", null=True, blank=True)
+    category = models.ForeignKey(
+        CategoryModel, on_delete=models.CASCADE, related_name="books"
+    )
