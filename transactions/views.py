22c27770f6f0dac66c9213d3d8cb70af8b86f0f0
sakayat <sakhyatkabir1@gmail.com>
2024-06-18 10:45:59 +0600

    transactions mixin

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import TransactionsModel
 # Create your views here.
+class TransactionCreateMixin(LoginRequiredMixin, CreateView):
+    model = TransactionsModel
+    success_url = reverse_lazy("home")
+    
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({"amount": self.request.user.account})
+        return kwargs
+    
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+    
+    
