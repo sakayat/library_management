eee33666e73154be4129ac58fc322747dc4bd043
sakayat <sakhyatkabir1@gmail.com>
2024-06-18 10:31:38 +0600

    added transactions forms

---
+++ transactions/forms.py
@@ -0,0 +1,26 @@
+from typing import Any
+from django import forms
+from .models import TransactionsModel
+
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = TransactionsModel
+        fields = ["amount", "transactions_type"]
+
+    def __init__(self, *args, **kwargs):
+        self.account = kwargs.pop("account", None)
+        super().__init__(*args, **kwargs)
+        self.fields["transactions_type"].disable = True
+        self.fields["transactions_type"].widget = forms.HiddenInput()
+        for field in self.fields:
+            self.fields[field].widget.attrs.update(
+                {
+                    "class": "block w-full px-3.5 py-2 text-gray-900 outline-none border border-black"
+                }
+            )
+
+    def save(self, commit: True):
+        self.instance.account = self.account
+        self.instance.balance_after_purchase = self.account.balance
+        return super().save(commit)
